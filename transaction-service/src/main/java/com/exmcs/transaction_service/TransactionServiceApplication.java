a94b7acc9ab039b599c995d8f271511ed1ea5bd2
rahmat.h <rahmat.hidayatulloh2010@gmail.com>
2024-11-07 23:09:06 +0700

    enhance eureka server

---
+++ transaction-service/src/main/java/com/exmcs/transaction_service/TransactionServiceApplication.java
@@ -0,0 +1,21 @@
+package com.exmcs.transaction_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class TransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionServiceApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+}
