8d4d084ddf242de2435cadbf06eb41d0aeaad158
rahmat.h <rahmat.hidayatulloh2010@gmail.com>
2024-11-19 05:08:27 +0700

    msg: enhance login-service v2

---
+++ login-service/src/test/java/com/exmcs/login_service/LoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exmcs.login_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
